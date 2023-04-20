77c4b71e958d9e0f25ec22d51e3e0c0faf0f96d4
EtherealOnyx <EtherealOnyx@users.noreply.github.com>
2023-04-20 16:05:25 -0700

    Implemented more config options
    Starting support for other party mods and party persistence.

--- src/main/java/io/sedu/mc/parties/data/ServerConfigData.java
+++ src/main/java/io/sedu/mc/parties/data/ServerConfigData.java
@@ -13,9 +13,27 @@ public class ServerConfigData {
     public static ForgeConfigSpec.BooleanValue friendlyFire;
     public static ForgeConfigSpec.BooleanValue globalShare;
     public static ForgeConfigSpec.BooleanValue enableShare;
+    public static ForgeConfigSpec.EnumValue<PartySync> partiesSyncType;
+    public static ForgeConfigSpec.BooleanValue partyPersistence;
+
+
+    enum PartySync {
+        NONE,
+        OPEN_PAC,
+        PARTIES,
+      //FTB_TEAMS
+    }
 
     public static void registerServerConfig(ForgeConfigSpec.Builder SERVER_BUILDER) {
         SERVER_BUILDER.comment("Server Party Settings").push("party");
+
+        partiesSyncType = SERVER_BUILDER.comment("This allows for integration with other party mods. Any option aside from 'NONE' forces partyPersistence to true. The following options are:",
+                                                 "NONE     - Ignore other party mods and use the Parties Mod as is. ",
+                                                 "OPEN_PAC - Relies on Open-PAC for parties. This disables all commands from Parties and forces it to sync with Open_PAC.",
+                                                 "PARTIES  - Relies on Parties for parties. This makes Open_PAC parties sync with this mod.")
+                                        .defineEnum("partiesSyncType", PartySync.NONE);
+        partyPersistence = SERVER_BUILDER.comment("Makes parties persist on server launches. Disabling this removes all saved parties - they will have to be remade.")
+                                         .define("partyPersistence", true);
 
         playerUpdateInterval = SERVER_BUILDER.comment("Delay (in ticks) for player packet syncing (hunger, xp)")
                                              .defineInRange("playerUpdateInterval", 10, 10, 200);
@@ -33,5 +51,18 @@ public class ServerConfigData {
                                     .define("globalShare", true);
 
         SERVER_BUILDER.pop();
+    }
+
+    public static boolean isPersistEnabled() {
+        if (partyPersistence.get())
+            return true;
+        else {
+            if (partiesSyncType.get() != PartySync.NONE) {
+                partyPersistence.set(true);
+                partyPersistence.save();
+                return true;
+            }
+        }
+        return false;
     }
 }
