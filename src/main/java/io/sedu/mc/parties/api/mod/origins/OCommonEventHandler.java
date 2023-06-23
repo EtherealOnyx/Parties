9eb881fa34c83495f184748f3bdabaf1203b6ab3
EtherealOnyx <EtherealOnyx@users.noreply.github.com>
2023-06-23 14:49:50 -0700

    Completed support for Origins, #146

--- src/main/java/io/sedu/mc/parties/api/mod/origins/OCommonEventHandler.java
+++ src/main/java/io/sedu/mc/parties/api/mod/origins/OCommonEventHandler.java
@@ -1,12 +1,14 @@
 package io.sedu.mc.parties.api.mod.origins;
 
-import io.sedu.mc.parties.Parties;
 import io.sedu.mc.parties.api.events.PartyJoinEvent;
 import io.sedu.mc.parties.api.helper.PlayerAPI;
 import io.sedu.mc.parties.data.PlayerData;
 import io.sedu.mc.parties.network.InfoPacketHelper;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class OCommonEventHandler {
     @SubscribeEvent
@@ -17,7 +19,18 @@ public class OCommonEventHandler {
     @SubscribeEvent
     public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
         String o = OCompatManager.getHandler().getMainOrigin(event.getPlayer());
-        Parties.LOGGER.debug(o);
         InfoPacketHelper.sendOriginUpdate(event.getPlayer().getUUID(), o);
+    }
+
+    public static void changeOrigin(UUID propOf, String origin) {
+        if (PlayerData.playerList.get(propOf) == null) return;
+        PlayerData.playerList.get(propOf).setOrigin(origin, () -> {
+            InfoPacketHelper.sendOriginUpdate(propOf, origin);
+            HashMap<UUID, Boolean> trackers;
+            if ((trackers = PlayerData.playerTrackers.get(propOf)) != null) {
+                trackers.forEach((id, serverTracked) -> InfoPacketHelper.sendOriginUpdate(id, propOf, origin));
+            }
+        });
+
     }
 }
---
+++ src/main/java/io/sedu/mc/parties/mixin/ChooseOriginMixin.java
@@ -0,0 +1,40 @@
+package io.sedu.mc.parties.mixin;
+
+import io.github.edwinmindcraft.origins.api.origin.Origin;
+import io.github.edwinmindcraft.origins.api.origin.OriginLayer;
+import io.github.edwinmindcraft.origins.common.capabilities.OriginContainer;
+import io.sedu.mc.parties.Parties;
+import io.sedu.mc.parties.api.mod.origins.OCommonEventHandler;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Objects;
+
+@Mixin(OriginContainer.class)
+public abstract class ChooseOriginMixin {
+
+    @Final
+    @Shadow
+    private Player player;
+
+    @Inject(method="setOrigin", at = @At(value = "TAIL"), remap = false, locals = LocalCapture.CAPTURE_FAILHARD)
+    private void originChangeListener(OriginLayer layer, Origin origin, CallbackInfo ci, Origin previous) {
+        if (player != null && origin != Origin.EMPTY) {
+            if (previous != null) {
+                if (previous.getOrder() >= origin.getOrder()) {
+                    Parties.LOGGER.debug("{} has been assigned a new main origin: {}", player.getScoreboardName(), origin.getRegistryName());
+                    OCommonEventHandler.changeOrigin(player.getUUID(), Objects.requireNonNull(origin.getRegistryName()).toString());
+                }
+            } else {
+                Parties.LOGGER.debug("{} has been assigned a new main origin: {}", player.getScoreboardName(), origin.getRegistryName());
+                OCommonEventHandler.changeOrigin(player.getUUID(), Objects.requireNonNull(origin.getRegistryName()).toString());
+            }
+        }
+    }
+}
